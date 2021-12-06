ecdc51aaa33d2cb190ea1a946ba148851240a8b3
Levente Novotny <Novotny.Levente@gmail.com>
2021-12-06 20:24:07 +0100

    Day6 Part2

--- Day6/06.cpp
+++ Day6/06.cpp
@@ -4,16 +4,19 @@
 #include <vector>
 #include <iostream>
 
+void partTwo(std::vector<int>);
 
 //Part 1 only works with smaller numbers
 int main() {
 	std::string line;
 	std::ifstream file("06.txt");
-	std::vector<int> fishes = {};
+	std::vector<int> originalFishes = {};
 
 	while (std::getline(file, line, ',')) {
-		fishes.push_back(std::stoi(line));
+		originalFishes.push_back(std::stoi(line));
 	}
+	std::vector<int> fishes = originalFishes;
+	
 
 	for (int i = 1; i <= 80; i++) {
 		int fishSize = fishes.size();
@@ -25,7 +28,47 @@ int main() {
 				fishes[fishIndex] -= 1;
 			}
 		}
-		std::cout << "After " << i << " days there will be " << fishes.size() << " fishes" << std::endl;
 	}
+	std::cout << "After " << 80 << " days there will be " << fishes.size() << " fishes" << std::endl;
+
+	std::cout << "To run second part press enter:" << std::endl << std::endl;
+	getchar();
+	partTwo(originalFishes);
 	return 0;
 }
+
+void partTwo(std::vector<int> inp) {
+	std::cout << inp.size();
+	std::vector<long long> fishes = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	// Load initial values
+	for (int i = 0; i < inp.size(); i++) {
+		fishes[inp[i]]++;
+	}
+	// Iterate over days
+	for (int i = 1; i <= 256; i++) {
+		long long addFishCount = fishes[0];
+
+		fishes[0] = fishes[1];
+		fishes[1] = fishes[2];
+		fishes[2] = fishes[3];
+		fishes[3] = fishes[4];
+		fishes[4] = fishes[5];
+		fishes[5] = fishes[6];
+		fishes[6] = fishes[7];
+		fishes[7] = fishes[8];
+
+		fishes[6] += addFishCount;
+		fishes[8] = addFishCount;
+
+		if (i == 180) {
+			std::cout << "alma";
+		}
+	}
+
+	std::cout << "After " << 256 << " days there will be ";
+	long long sum = 0;
+	for (int i = 0; i < fishes.size(); i++) {
+		sum += fishes[i];
+	}
+	std::cout << sum << " fishes" << std::endl;
+};
