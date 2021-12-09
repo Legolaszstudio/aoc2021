f3f78c6cb03175f0616ed57e294ffe38ac318c60
Levente Novotny <Novotny.Levente@gmail.com>
2021-12-09 17:09:42 +0100

    Day9 part1

---
+++ Day9/09.cpp
@@ -0,0 +1,55 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <deque>
+#include <vector>
+#include <iostream>
+
+int main() {
+	std::string line;
+	std::ifstream file("09.txt");
+	std::vector<std::vector<int>> inputMatrix = {};
+
+	while (std::getline(file, line)) {
+		inputMatrix.push_back({});
+		int lastIndex = inputMatrix.size() - 1;
+		for (int i = 0; i < line.size(); i++) {
+			inputMatrix[lastIndex].push_back(std::stoi(std::string(1,line[i])));
+		}
+	}
+
+	int riskLevel = 0;
+	for (int i = 0; i < inputMatrix.size(); i++) {
+		for (int j = 0; j < inputMatrix[i].size(); j++) {
+			int currentNum = inputMatrix[i][j];
+			if (i != 0) {
+				// we can check upwards
+				if (currentNum >= inputMatrix[i - 1][j]) {
+					continue;
+				}
+			}
+			if (i != inputMatrix.size() - 1) {
+				// we can check downwards
+				if (currentNum >= inputMatrix[i + 1][j]) {
+					continue;
+				}
+			}
+			if (j != 0) {
+				// we can check left
+				if (currentNum >= inputMatrix[i][j - 1]) {
+					continue;
+				}
+			}
+			if (j != inputMatrix[i].size() - 1) {
+				// we can check right
+				if (currentNum >= inputMatrix[i][j + 1]) {
+					continue;
+				}
+			}
+			std::cout << currentNum << " ";
+			riskLevel += currentNum + 1;
+		}
+	}
+	std::cout << std::endl << "Final risk level: " << riskLevel;
+	return 0;
+}
