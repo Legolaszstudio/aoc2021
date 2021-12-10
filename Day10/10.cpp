860e77e624bf3d088d62c6628b4e174e606f1401
Levente Novotny <Novotny.Levente@gmail.com>
2021-12-10 16:53:44 +0100

    Day10 Part1

--- Day10/10.cpp
+++ Day10/10.cpp
@@ -1,3 +1,46 @@
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <deque>
+#include <vector>
+#include <iostream>
+
 int main() {
+	std::string line;
+	std::ifstream file("10.txt");
+
+	int points = 0;
+	while (std::getline(file, line)) {
+		std::deque<char> requiredClosingTags = {};
+		for(int i = 0; i < line.size(); i++) {
+			char currentTag = line[i];
+			if (currentTag == '(') {
+				requiredClosingTags.push_front(')');
+			} else if (currentTag == '[') {
+				requiredClosingTags.push_front(']');
+			} else if (currentTag == '{') {
+				requiredClosingTags.push_front('}');
+			} else if (currentTag == '<') {
+				requiredClosingTags.push_front('>');
+			} else {
+				if (requiredClosingTags[0] != currentTag) {
+					if (currentTag == ')') {
+						points += 3;
+					} else if (currentTag == ']') {
+						points += 57;
+					} else if (currentTag == '}') {
+						points += 1197;
+					} else if (currentTag == '>') {
+						points += 25137;
+					}
+					break;
+				} else {
+					requiredClosingTags.erase(requiredClosingTags.begin());
+				}
+			}
+		}
+	}
+
+	std::cout << "Final syntax error points: " << points;
 	return 0;
 }
