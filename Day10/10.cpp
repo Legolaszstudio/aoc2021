cb7a1dd4c514be267243459e73fa05357ee9a002
Levente Novotny <Novotny.Levente@gmail.com>
2021-12-10 17:13:13 +0100

    Day10 Part2

--- Day10/10.cpp
+++ Day10/10.cpp
@@ -4,15 +4,17 @@
 #include <deque>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 int main() {
 	std::string line;
 	std::ifstream file("10.txt");
 
 	int points = 0;
+	std::vector<long long> syntaxErrorPoints = {};
 	while (std::getline(file, line)) {
 		std::deque<char> requiredClosingTags = {};
-		for(int i = 0; i < line.size(); i++) {
+		for (int i = 0; i < line.size(); i++) {
 			char currentTag = line[i];
 			if (currentTag == '(') {
 				requiredClosingTags.push_front(')');
@@ -33,14 +35,31 @@ int main() {
 					} else if (currentTag == '>') {
 						points += 25137;
 					}
+					requiredClosingTags = {};
 					break;
 				} else {
 					requiredClosingTags.erase(requiredClosingTags.begin());
 				}
 			}
 		}
+		long long closingTagPoints = 0;
+		for (int i = 0; i < requiredClosingTags.size(); i++) {
+			char closingTag = requiredClosingTags[i];
+			if (closingTag == ')') {
+				closingTagPoints = (closingTagPoints * 5) + 1;
+			} else if (closingTag == ']') {
+				closingTagPoints = (closingTagPoints * 5) + 2;
+			} else if (closingTag == '}') {
+				closingTagPoints = (closingTagPoints * 5) + 3;
+			} else if (closingTag == '>') {
+				closingTagPoints = (closingTagPoints * 5) + 4;
+			}
+		}
+		if (closingTagPoints != 0) syntaxErrorPoints.push_back(closingTagPoints);
 	}
 
-	std::cout << "Final syntax error points: " << points;
+	std::cout << "Final corrupted line points: " << points << std::endl;
+	std::sort(syntaxErrorPoints.begin(), syntaxErrorPoints.end());
+	std::cout << "Final incomplete lines points: " << syntaxErrorPoints[syntaxErrorPoints.size() / 2] << std::endl;
 	return 0;
 }
